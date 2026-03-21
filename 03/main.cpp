10501495813a43ce240ee841046fa0499740309e
Skry <52260762+Skry@users.noreply.github.com>
2026-03-21 22:48:28 +0100

    update

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,9 +1,15 @@
 #include "io.hpp"
+#include <iostream>
+#define DEBUG
 
-int main()
-{
-    int x{readNumber()};
-    int y{readNumber()};
-    writeAnswer(x + y);
-    return 0;
+int main() {
+
+#ifdef DEBUG
+  std::cout << std::unitbuf; // enable automatic flushing for std::cout
+#endif
+
+  int x{readNumber()};
+  int y{readNumber()};
+  writeAnswer(x + y);
+  return 0;
 }
