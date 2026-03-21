7983a01744992d50cf88e5807eca445313c0dffa
Skry <52260762+Skry@users.noreply.github.com>
2026-03-21 18:51:15 +0100

    up

---
+++ 03/io.hpp
@@ -0,0 +1,7 @@
+#ifndef HEADER1
+#define HEADER1
+
+int readNumber();
+void writeAnswer(int x);
+
+#endif
--- 03/main.cpp
+++ 03/main.cpp
@@ -1,26 +1,9 @@
-#include <iostream>
-
-int readNumber();
-void writeAnswer(int x);
-
+#include "io.hpp"
 
 int main()
 {
     int x { readNumber() };
     int y { readNumber() };
-    writeAnswer(x + y); // using operator+ to pass the sum of x and y to writeAnswer()
+    writeAnswer(x + y);
     return 0;
-}
-
-int readNumber()
-{
-    std::cout << "Enter a number to add: ";
-    int x {};
-    std::cin >> x;
-    return x;
-}
-
-void writeAnswer(int x)
-{
-    std::cout << "The answer is " << x << '\n';
 }
