16b2858b8956dd052fb5c0b67643567f8e10c3c3
Skry <52260762+Skry@users.noreply.github.com>
2026-03-23 21:57:38 +0100

    Up

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 
-int main() {
+bool isPrime(int x) {
+  if (x == 2) // if user entered 2, the digit is prime
+    return true;
+  else if (x == 3) // if user entered 3, the digit is prime
+    return true;
+  else if (x == 5) // if user entered 5, the digit is prime
+    return true;
+  else if (x == 7) // if user entered 7, the digit is prime
+    return true;
 
+  return false; // if the user did not enter 2, 3, 5, 7, the digit must not be
+                // prime
+}
+
+int getNumber() {
+  std::cout << "Enter a value between 0 and 9" << ".\n";
   int x{};
   std::cin >> x;
+  return x;
+}
 
-  if (x > 5) {
-    std::cout << "La valeur est au dessus de 5" << ".\n";
-    return 0;
+int main() {
+
+  int x{getNumber()};
+
+  if (x < 0 || x > 9) {
+    x = getNumber();
   }
 
-  std::cout << "La valeur est en dessous de 6" << ".\n";
+  if (isPrime(x)) {
+    std::cout << "Value is prime" << ".\n";
+  } else {
+    std::cout << "Value is not prime" << ".\n";
+  }
 
   return 0;
 }
